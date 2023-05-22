85c29bae02ce45433a7ef0cbe5d44e01a44d8925
guiguididi <guiguididi@imac-de-guiguididi-1.home>
2023-05-22 23:12:06 +0200

    ✨ add .filter search + 🚧 filter data with graphQl to optimise

--- src/components/Continents.tsx
+++ src/components/Continents.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery, gql } from "@apollo/client";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Card } from "antd";
 const { Meta } = Card;
 
@@ -18,15 +18,56 @@ const GET_CONTINENTS = gql`
   }
 `;
 
+// const GET_CONTINENTS = gql`
+//   query getContinents($filter: ContinentFilterInput) {
+//     continents(filter: $filter) {
+//       code
+//       name
+//     }
+//   }
+// `;
+
 const Continents = () => {
+  //const [searchTerm, setSearchTerm] = useState("");
+
+  const [query, setQuery] = useState("");
+
   const { loading, error, data } = useQuery(GET_CONTINENTS);
   console.log("======== data :", data);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
+  // const { loading, error, data, refetch } = useQuery(GET_CONTINENTS, {
+  //   variables: {
+  //     filter: { code: { eq: searchTerm } },
+  //   },
+  // });
+  // if (loading) return <p>Loading...</p>;
+  // if (error) return <p>Error :(</p>;
+
+  // const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setSearchTerm(e.target.value);
+  // };
+
   return (
     <div className="flex justify-center gap-8">
-      {data.continents.map((continent: Continent, index: number) => (
+      {/* <div>
+        <label>Search</label>
+        <input type="text" value={searchTerm} onChange={handleSearch} />
+      </div> */}
+
+      <div>
+        <label>Search</label>
+        <input
+          className="search"
+          placeholder="Search..."
+          onChange={(e) => setQuery(e.target.value.toLowerCase())}
+        />
+      </div>
+
+      {data.continents
+        .filter((continent:any) => continent.name.toLowerCase().includes(query))
+        .map((continent: Continent, index: number) => (
         <Link
           key={index}
           to={`/continent/${continent.code}`}
